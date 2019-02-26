44abe00c305c4cde82fb2b9cedbccf6655a3a317
Jansen <senducusin@gmail.com>
2019-02-27 05:51:42 +0800

    toggle appliance + redirect when pin is toggled

---
+++ TogglePin.ino
@@ -0,0 +1,6 @@
+void TogglePin() {
+  digitalWrite(appliancePin, !digitalRead(appliancePin));
+  delay(1);
+  server.sendHeader("Location", String("/"), true);
+  server.send ( 302, "text/plain", "");
+}
--- boilerplate-ESP8266-tqui.ino
+++ boilerplate-ESP8266-tqui.ino
@@ -5,6 +5,8 @@
 
 // Pin definition
 #define lm35Pin A0
+#define appliancePin 2
+#define readPin 0
 
 // Replace with your network credentials
 const char* ssid = "home";//"<Your WiFI SSID>";
@@ -12,7 +14,7 @@ const char* password = "Pqwe^&8rtYu54B7*@@#";//"<Your WiFI Password>";
 
 // Millis Timer
 unsigned long previousMillis = 0;
-const long interval = 500;
+const long interval = 50;
 
 // Web UI variables
 String page = "";
@@ -21,11 +23,16 @@ String text = "";
 // Logic variables
 int tempValue = 0;
 float cel;
+String appStatus = "OFF";
 
 //instantiate server at port 80 (http port)
 ESP8266WebServer server(80);
 
 void setup() {
+  // Pin Initialization
+  pinMode(appliancePin, OUTPUT);
+  pinMode(readPin, INPUT);
+
   Serial.begin(9600);
   delay(1);
   WiFi.begin(ssid, password); //begin WiFi connection
@@ -44,23 +51,24 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   server.on("/data.txt", []() {
-    text = (String)cel + "|" + (String)true;
+    text = (String)cel + "|" + appStatus;
     server.send(200, "text/html", text);
   });
 
   server.on("/", []() {
-    
+
     page = "<h1>ESP8266 Web UI + AJAX Boilerplate</h1>\n";
     page += "<h2>Analog Check</h2>\n";
     page += "<h3 style=\"display:inline-block; margin-right:5px;\">Temperature:</h3>\n";
     page += "<h3 style=\"display:inline-block;\" id=\"analog-data\">""</h3>\r\n";
+    page += "<a href=\"toggleLink\">Toggle Appliance</a>\n";
     page += "<h2>Digital Check</h2>\n";
     page += "<h3 style=\"display:inline-block; margin-right:5px;\">Appliance:</h3>\n";
     page += "<h3 style=\"display:inline-block;\" id=\"digital-data\">""</h3>\r\n";
-    
+
     page += "<script>\r\n";
     page += "var x = setInterval(function() {loadData(\"data.txt\",updateData)}, 1000);\r\n";
-    
+
     page += "function loadData(url, callback){\r\n";
     page += "var xhttp = new XMLHttpRequest();\r\n";
     page += "xhttp.onreadystatechange = function(){\r\n";
@@ -71,7 +79,7 @@ void setup() {
     page += "xhttp.open(\"GET\", url, true);\r\n";
     page += "xhttp.send();\r\n";
     page += "}\r\n";
-    
+
     page += "function updateData(){\r\n";
     page += "let data = this.responseText.split('|')\n";
     page += "let temp = data[0]\n";
@@ -80,9 +88,11 @@ void setup() {
     page += "document.getElementById(\"digital-data\").innerHTML = relay;\r\n";
     page += "}\r\n";
     page += "</script>\r\n";
- 
+
     server.send(200, "text/html", page);
   });
+
+  server.on("/toggleLink", TogglePin);
   server.begin();
   Serial.println("Web server started!");
 }
@@ -91,10 +101,18 @@ void loop() {
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-    
+
     int analogValue = analogRead(lm35Pin);
     cel = getCelsius(analogValue);
-    
+
+    int digitalValue = digitalRead(appliancePin);
+
+    if (digitalValue == HIGH) {
+      appStatus = "ON";
+    } else {
+      appStatus = "OFF";
+    }
+
     server.handleClient();
   }
 }
